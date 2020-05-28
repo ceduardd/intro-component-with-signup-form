00f52b040315980a7fd611b94d70eab072ef76fc
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-05-28 15:03:44 -0500

    version responsive added 📱

--- src/index.js
+++ src/index.js
@@ -1,66 +1,54 @@
 const form = document.getElementById('form');
-const firstName = document.getElementById('firstname');
-const lastName = document.getElementById('lastname');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+form.addEventListener('submit', e => {
+	e.preventDefault();
 
-  checkInputs();
+	const firstName = form['firstname'].value;
+	const lastName = form['lastname'].value;
+	const email = form['email'].value;
+	const password = form['password'].value;
+
+	if (firstName === '') {
+		addErrorTo('firstname', 'First Name is required');
+	} else {
+		removeErrorFrom('firstname');
+	}
+
+	if (lastName === '') {
+		addErrorTo('lastname', 'Last Name is required');
+	} else {
+		removeErrorFrom('lastname');
+	}
+
+	if (email === '') {
+		addErrorTo('email', 'Email is required');
+	} else if (!isValid(email)) {
+		addErrorTo('email', 'Email is not valid');
+	} else {
+		removeErrorFrom('email');
+	}
+
+	if (password === '') {
+		addErrorTo('password', 'Password is required');
+	} else {
+		removeErrorFrom('password');
+	}
 });
 
-function checkInputs() {
-  // trim to remove whitespaces
-  const firstNameValue = firstName.value.trim();
-  const lastNameValue = lastName.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
+function addErrorTo(field, message) {
+	const formControl = form[field].parentNode;
+	formControl.classList.add('error');
 
-  // check the validity of the form fields
-  if (firstNameValue === '') {
-    setErrorFor(firstName, 'First Name cannot be empty');
-  } else {
-    setSuccessFor(firstName);
-  }
-
-  if (lastNameValue === '') {
-    setErrorFor(lastName, 'Last Name cannot be empty');
-  } else {
-    setSuccessFor(lastName);
-  }
-
-  if (emailValue === '') {
-    setErrorFor(email, 'Email Address cannot be empty');
-  } else if (!isEmail(emailValue)) {
-    setErrorFor(email, 'Looks like this is not an email');
-  } else {
-    setSuccessFor(email);
-  }
-
-  if (passwordValue === '') {
-    setErrorFor(password, 'Password cannot be empty');
-  } else {
-    setSuccessFor(password);
-  }
+	const small = formControl.querySelector('small');
+	small.innerText = message;
 }
 
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  formControl.className = 'form-control error';
-
-  small.innerText = message;
+function removeErrorFrom(field) {
+	const formControl = form[field].parentNode;
+	formControl.classList.remove('error');
 }
 
-function setSuccessFor(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control';
-}
-
-function isEmail(email) {
-  // regex expression to validate email
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
+function isValid(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
